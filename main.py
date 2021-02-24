7c1574f7b7d9ec777907fd457063df3f4d7aad1c
Vishal sepaia <vishal.thakur607@gmail.com>
2021-02-24 17:04:40 +0530

    branch for japanese

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from tkinter import *
 import pandas
 from random import choice
+#import xlrd
+#import openpyxl
 
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 data_dict ={}
 #Data
 try:
-    data = pandas.read_csv("data/words_to_learn.csv")
+    data = pandas.read_excel("data/words_to_learn.xlsx")
 except FileNotFoundError:
-    orignal_data = pandas.read_csv("data/french_words.csv")
+    orignal_data = pandas.read_excel("data/japaneseFrequencylist.xlsx")
     data_dict = orignal_data.to_dict(orient="records")
 else:
     data_dict = data.to_dict(orient= "records")
@@ -20,8 +22,8 @@ def next_word():
     window.after_cancel(flip_card_timer)
     current_card = choice(data_dict)
     canvas.itemconfig(canvas_image, image = card_front_image)
-    canvas.itemconfig(card_title, text="French", fill = "black")
-    canvas.itemconfig(card_word, text = current_card["French"], fill = "black")
+    canvas.itemconfig(card_title, text="Japanese", fill = "black")
+    canvas.itemconfig(card_word, text = current_card["Japanese"], fill = "black")
     flip_card_timer = window.after(3000, func=word_meaning)
 
 
@@ -35,7 +37,7 @@ def word_meaning():
 def is_known():
     data_dict.remove(current_card)
     data = pandas.DataFrame(data_dict)
-    data.to_csv("data/words_to_learn.csv", index= False)
+    data.to_excel("data/words_to_learn.xlsx", index= False)
     next_word()
 
 
@@ -52,7 +54,7 @@ card_front_image = PhotoImage(file = "images/card_front.png")
 card_back_image = PhotoImage(file="images/card_back.png")
 canvas_image = canvas.create_image(400, 263, image = card_front_image)
 card_title = canvas.create_text(400, 150, text = "", font = ("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text = "", font = ("Ariel", 60, "bold"))
+card_word = canvas.create_text(400, 263, text = "", font = ("Ariel", 40, "bold"))
 canvas.grid(row = 0, column = 0, columnspan =2)
 
 
